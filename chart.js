5c0301c21c607ff295b960b017dcc9a16935d217
agengnagel <adriennegengnagel@hotmail.com>
2016-02-24 07:56:20 -0800

    started chart.js

---
+++ chart.js
@@ -0,0 +1,15 @@
+var myBarChart= new Chart(ctx).Bar(data,options)
+
+var barData = {
+  labels: ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dogduck', 'dragon', 'pen', 'petsweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'watercan', 'wineglass'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fillColor: 'rgba(220,220,220,0.5)',
+      strokeColor: 'rgba(220,220,220,0.8)',
+      highlightFill: 'rgba(220,220,220,0.75)',
+      highlightStroke: 'rgba(220,220,220,1)',
+      data: arrayRandoms
+    },
+  ]
+};
